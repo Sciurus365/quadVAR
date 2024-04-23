f66dc2f35d273ce970857e97ff62412bce45f0e0
Sciurus365 <jingmeng.cui@outlook.com>
2024-04-23 15:48:10 +0200

    24.4.23.1
    get back the original, not zero-centered model (from PROJ_2023_nonlinear_networks, 23.12.19.2)

--- R/sim-models.R
+++ R/sim-models.R
@@ -10,7 +10,7 @@
 #'
 #' @seealso [true_model_4_emo()], [compare_4_emo()], [quadVAR()]
 #' @export
-sim_4_emo <- function(time = 200, init = c(1.3, 1.3, 4.8, 4.8), sd = 0.2) {
+sim_4_emo <- function(time = 200, init = c(1.3, 1.3, 4.8, 4.8), sd = 1) {
   output <- matrix(nrow = time, ncol = 5)
   colnames(output) <- c("time", "x1", "x2", "x3", "x4")
   output[, 1] <- 1:time
@@ -20,11 +20,10 @@ sim_4_emo <- function(time = 200, init = c(1.3, 1.3, 4.8, 4.8), sd = 0.2) {
     x2 <- output[i - 1, 3]
     x3 <- output[i - 1, 4]
     x4 <- output[i - 1, 5]
-    x1_1 <- x1 - 0.22 - 0.64 * x1 - 0.1 * x1^2 + 0.06 * x2 + 0.02 * x1 * x2 - 0.3 * x3 - 0.1 * x1 * x3 - 0.3 * x4 - 0.1 * x1 * x4 + stats::rnorm(1, sd = sd)
-    x2_1 <- x2 - 0.22 - 0.64 * x2 - 0.1 * x2^2 + 0.06 * x1 + 0.02 * x1 * x2 - 0.3 * x3 - 0.1 * x2 * x3 - 0.3 * x4 - 0.1 * x2 * x4 + stats::rnorm(1, sd = sd)
-    x3_1 <- x3 - 0.22 - 0.64 * x3 - 0.1 * x3^2 + 0.06 * x4 + 0.02 * x3 * x4 - 0.3 * x1 - 0.1 * x1 * x3 - 0.3 * x2 - 0.1 * x2 * x3 + stats::rnorm(1, sd = sd)
-    x4_1 <- x4 - 0.22 - 0.64 * x4 - 0.1 * x4^2 + 0.06 * x3 + 0.02 * x3 * x4 - 0.3 * x1 - 0.1 * x1 * x4 - 0.3 * x2 - 0.1 * x2 * x4 + stats::rnorm(1, sd = sd)
-
+    x1_1 <- 0.8 + 1.5*x1 - 0.1*x1^2 +0.02*x1*x2-0.1*x1*x3-0.1*x1*x4 + stats::rnorm(1, sd = sd)
+    x2_1 <- 0.8 + 1.5*x2 - 0.1*x2^2 +0.02*x1*x2-0.1*x2*x3-0.1*x2*x4 + stats::rnorm(1, sd = sd)
+    x3_1 <- 0.8 + 1.5*x3 - 0.1*x3^2 +0.02*x3*x4-0.1*x1*x3-0.1*x2*x3 + stats::rnorm(1, sd = sd)
+    x4_1 <- 0.8 + 1.5*x4 - 0.1*x4^2 +0.02*x3*x4-0.1*x1*x4-0.1*x2*x4 + stats::rnorm(1, sd = sd)
     output[i, 2:5] <- c(x1_1, x2_1, x3_1, x4_1)
   }
   return(output)
@@ -65,16 +64,16 @@ coef.true_model_4_emo <- function(object, silent = FALSE, ...) {
   for (i in 1:4) {
     # then fill in the true effects
     if (i == 1) {
-      output$estimate[output$model == i & output$effect %in% c("X1", "X2", "X3", "X4")] <- c(0.36, 0.06, -0.3, -0.3)
+      output$estimate[output$model == i & output$effect %in% c("X1")] <- c(1.5)
       output$estimate[output$model == i & output$effect %in% c("X1X1", "X1X2", "X1X3", "X1X4")] <- c(-0.1, 0.02, -0.1, -0.1)
     } else if (i == 2) {
-      output$estimate[output$model == i & output$effect %in% c("X1", "X2", "X3", "X4")] <- c(0.06, 0.36, -0.3, -0.3)
+      output$estimate[output$model == i & output$effect %in% c("X2")] <- c(1.5)
       output$estimate[output$model == i & output$effect %in% c("X1X2", "X2X2", "X2X3", "X2X4")] <- c(0.02, -0.1, -0.1, -0.1)
     } else if (i == 3) {
-      output$estimate[output$model == i & output$effect %in% c("X1", "X2", "X3", "X4")] <- c(-0.3, -0.3, 0.36, 0.06)
+      output$estimate[output$model == i & output$effect %in% c("X3")] <- c(1.5)
       output$estimate[output$model == i & output$effect %in% c("X1X3", "X2X3", "X3X3", "X3X4")] <- c(-0.1, -0.1, -0.1, 0.02)
     } else if (i == 4) {
-      output$estimate[output$model == i & output$effect %in% c("X1", "X2", "X3", "X4")] <- c(-0.3, -0.3, 0.06, 0.36)
+      output$estimate[output$model == i & output$effect %in% c("X4")] <- c(1.5)
       output$estimate[output$model == i & output$effect %in% c("X1X4", "X2X4", "X3X4", "X4X4")] <- c(-0.1, -0.1, 0.02, -0.1)
     }
   }
@@ -100,23 +99,23 @@ print.true_model_4_emo <- function(x, which = NULL, ...) {
       print(x, which = i)
     }
   } else if (which == 1) {
-    cat("True main effects: 1 2 3 4\n")
-    cat("Coefficients for main effects: 0.36, 0.06, -0.3, -0.3\n")
+    cat("True main effects: 1\n")
+    cat("Coefficients for main effects: 1.5\n")
     cat("True Interaction effects: X1X1 X1X2 X1X3 X1X4\n")
     cat("Coefficients for interaction effects: -0.1, 0.02, -0.1, -0.1\n")
   } else if (which == 2) {
-    cat("True main effects: 1 2 3 4\n")
-    cat("Coefficients for main effects: 0.06, 0.36, -0.3, -0.3\n")
+    cat("True main effects: 2\n")
+    cat("Coefficients for main effects: 1.5\n")
     cat("True Interaction effects: X1X2 X2X2 X2X3 X2X4\n")
     cat("Coefficients for interaction effects: 0.02, -0.1, -0.1, -0.1\n")
   } else if (which == 3) {
-    cat("True main effects: 1 2 3 4\n")
-    cat("Coefficients for main effects: -0.3, -0.3, 0.36, 0.06\n")
+    cat("True main effects: 3\n")
+    cat("Coefficients for main effects: 1.5\n")
     cat("True Interaction effects: X1X3 X2X3 X3X3 X3X4\n")
     cat("Coefficients for interaction effects: -0.1, -0.1, -0.1, 0.02\n")
   } else if (which == 4) {
-    cat("True main effects: 1 2 3 4\n")
-    cat("Coefficients for main effects: -0.3, -0.3, 0.06, 0.36\n")
+    cat("True main effects: 4\n")
+    cat("Coefficients for main effects: 1.5\n")
     cat("True Interaction effects: X1X4 X2X4 X3X4 X4X4\n")
     cat("Coefficients for interaction effects: -0.1, -0.1, 0.02, -0.1\n")
   }
@@ -151,16 +150,16 @@ compare_4_emo <- function(model, silent = FALSE) {
     }
     # then fill in the true effects
     if (i == 1) {
-      output$true[output$model == i & output$effect %in% c("X1", "X2", "X3", "X4")] <- c(0.36, 0.06, -0.3, -0.3)
+      output$true[output$model == i & output$effect %in% c("X1")] <- c(1.5)
       output$true[output$model == i & output$effect %in% c("X1X1", "X1X2", "X1X3", "X1X4")] <- c(-0.1, 0.02, -0.1, -0.1)
     } else if (i == 2) {
-      output$true[output$model == i & output$effect %in% c("X1", "X2", "X3", "X4")] <- c(0.06, 0.36, -0.3, -0.3)
+      output$true[output$model == i & output$effect %in% c("X2")] <- c(1.5)
       output$true[output$model == i & output$effect %in% c("X1X2", "X2X2", "X2X3", "X2X4")] <- c(0.02, -0.1, -0.1, -0.1)
     } else if (i == 3) {
-      output$true[output$model == i & output$effect %in% c("X1", "X2", "X3", "X4")] <- c(-0.3, -0.3, 0.36, 0.06)
+      output$true[output$model == i & output$effect %in% c("X3")] <- c(1.5)
       output$true[output$model == i & output$effect %in% c("X1X3", "X2X3", "X3X3", "X3X4")] <- c(-0.1, -0.1, -0.1, 0.02)
     } else if (i == 4) {
-      output$true[output$model == i & output$effect %in% c("X1", "X2", "X3", "X4")] <- c(-0.3, -0.3, 0.06, 0.36)
+      output$true[output$model == i & output$effect %in% c("X4")] <- c(1.5)
       output$true[output$model == i & output$effect %in% c("X1X4", "X2X4", "X3X4", "X4X4")] <- c(-0.1, -0.1, 0.02, -0.1)
     }
   }
